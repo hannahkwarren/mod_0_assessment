d961e822b2c0204b1379692781cc5b7120dd7578
Hannah Warren <hannahkwarren@gmail.com>
2021-09-03 14:37:31 -0400

    Add dinosaur hash to model_a

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+DINOSAUR = {
+  "name" => "Stegosaurus",
+  "diet" => "Herbivore",
+  "height_ft" => 10,
+  "year_discovered" => 1887,
+  "years_ago_millions" => 155,
+  "second_brain" => true,
+  "plates" => true,
+  "discovery_locations" => ["United States", "Portugal"],
+  "types" => ["stenops", "ungulatus", "sulcatus"]
+}
