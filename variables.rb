9ce1f173134545af72388a9d94e7219e638f9bce
Hannah Warren <hannahkwarren@gmail.com>
2021-09-03 14:27:33 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,20 @@
+#create movie and is_entertaining vars
+movie = "Chocolat"
+is_entertaining = true
+
+# reassign movie var to a new string
+movie = "Amelie"
+
+# create number vars and calculate product
+num_4 = 100
+num_6 = 3
+product = num_4 * num_6
+
+# create favorite_foods array
+favorite_foods = ["pizza", "chocolate", "pasta"]
+
+# remove the first item in favorite_foods array
+favorite_foods = favorite_foods.shift()
+
+#print length of the movie string to console
+puts movie.length
